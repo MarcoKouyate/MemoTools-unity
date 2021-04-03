719d1e38943475a8eaafe02bbf4fe2657a10376c
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-04-03 23:20:43 +0200

    Swap two Vector3

--- Utilities.cs
+++ Utilities.cs
@@ -7,5 +7,12 @@ namespace MemoTools {
         {
             return (Mathf.Abs(vector.x) > threshold || Mathf.Abs(vector.y) > threshold);
         }
+
+        static public void Swap(ref Vector3 vector1, ref Vector3 vector2)
+        {
+            Vector3 temp = vector1;
+            vector1 = vector2;
+            vector2 = temp;
+        }
     }
  }
