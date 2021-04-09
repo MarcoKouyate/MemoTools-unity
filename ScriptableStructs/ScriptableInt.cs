729843984df14ecd238104f0f6e959368be87c14
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-04-09 15:11:43 +0200

    Scriptable integers

---
+++ ScriptableStructs/ScriptableInt.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+namespace MemoTools { 
+    [CreateAssetMenu(menuName ="MemoTools/Scriptable Structs/Int", fileName ="Scriptable Int")]
+    public class ScriptableInt : ScriptableObject
+    {
+        public int value;
+    }
+}
