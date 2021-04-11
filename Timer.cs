45ba62bcf1abc5c2ce50aadf5786607bea116803
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-04-11 02:09:35 +0200

    Timer utility class

---
+++ Timer.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+
+namespace MemoTools { 
+    public class Timer
+    {
+        public Timer(float interval, bool repeat)
+        {
+            _interval = interval;
+            _repeat = repeat;
+            _active = true;
+        }
+
+        public bool IsExpired
+        { 
+            get => Time.time > _nextTime;
+        }
+
+        public void Reset()
+        {
+            _nextTime = Time.time + _interval;
+            _active = true;
+        }
+
+        public bool OnTimeEnd
+        {
+            get
+            {
+                if (_active && IsExpired)
+                {
+                    _active = false;
+                    if (_repeat) Reset();
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        private bool _active;
+        private bool _repeat;
+        private float _nextTime;
+        private float _interval;
+    }
+}
