a237e750ef76cc540a75b87a97b2be9b5e34a6e8
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-04-13 13:10:15 +0200

    Scriptable Vector2

---
+++ ScriptableStructs/ScriptableVector2.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+namespace MemoTools { 
+    [CreateAssetMenu(menuName ="MemoTools/Scriptable Structs/Vector2", fileName ="Scriptable Vector2")]
+    public class ScriptableVector2 : ScriptableObject
+    {
+        public Vector2 value;
+    }
+}
